42746b9192f688df20c7704de6655e0851437c13
Aria Stewart <aredridel@dinhe.net>
2016-01-03 20:31:20 -0500

    Port test/post_length_keep_alive to through2

--- test/post_length_keep_alive.js
+++ test/post_length_keep_alive.js
@@ -2,7 +2,7 @@ var test = require('tap').test;
 var bouncy = require('../');
 var http = require('http');
 var net = require('net');
-var through = require('through');
+var through = require('through2');
 
 test('POST with keep-alive and content-length', function (t) {
     t.plan(2);
@@ -18,11 +18,15 @@ test('POST with keep-alive and content-length', function (t) {
     var s = http.createServer(function (req, res) {
         var size = 0;
         req.pipe(through(write, end));
-        function write (buf) { size += buf.length }
-        function end () {
+        function write (buf, _, cb) {
+            size += buf.length
+            cb()
+        }
+        function end (cb) {
             var s = String(size);
             res.setHeader('content-length', s.length);
             res.end(s)
+            cb()
         }
     });
     s.listen(0);
@@ -38,12 +42,13 @@ test('POST with keep-alive and content-length', function (t) {
         ].join('\r\n'));
         
         var data = '';
-        c.pipe(through(function (buf) {
+        c.pipe(through(function (buf, _, cb) {
             data += buf;
             if (/\r\n\r\n|\n\n/.test(data)) {
                 t.equal(data.split('\n').slice(-1)[0], '9');
                 c.end();
             }
+            cb();
         }));
     });
 });
